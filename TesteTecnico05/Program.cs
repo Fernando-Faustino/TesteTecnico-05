02a6be34294d36612e3767236e05e72c47b093b5
Fernando-Faustino <fernandofaustino627@gmail.com>
2023-04-08 20:41:01 -0300

    Adicionar arquivos de projeto.

---
+++ TesteTecnico05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace testeTecnico05 {
+    class Program {
+        static void Main(string[] args) {
+            static void Main(string[] args) {
+                string original = "exemplo"; // string original
+                string invertida = ""; // string invertida
+
+                for (int i = original.Length - 1; i >= 0; i--) {
+                    invertida += original[i];
+                }
+
+                Console.WriteLine("String original: {0}", original);
+                Console.WriteLine("String invertida: {0}", invertida);
+            }
+        }
+    }
+}
